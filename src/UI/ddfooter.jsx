3081bdd8e21caafaaf4a28e5f4b4fa5d353716fe
Karan <126536256+Karan-DD@users.noreply.github.com>
2023-03-28 22:13:33 -0400

    added dark background

--- src/UI/ddfooter.jsx
+++ src/UI/ddfooter.jsx
@@ -1,5 +1,6 @@
 import { Grid } from "antd";
 import { Footer } from "antd/es/layout/layout";
+import { colors } from "../../constants";
 import { useThemeStore } from "../store/themestore";
 
 const DDFooter = () => {
@@ -9,8 +10,9 @@ const DDFooter = () => {
 
   const style = {
     textAlign: "center",
-    paddingLeft: !mobile ? 350 : "",
+    paddingLeft: !mobile ? 300 : "",
     color: theme === "dark" ? "white" : "",
+    backgroundColor: theme === "dark" ? colors.darkMode : colors.lightMode,
   };
 
   return <Footer style={style}>Data ©2023 Created by Drawbridge Data</Footer>;
